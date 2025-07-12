50be2ff58d610667c2bc8a9bfd52c2128e3ef8c1
--unset <iskschluter@gmail.com>
2025-07-11 23:16:23 -0300

    cached user check

--- backend/src/middleware/isAuth.ts
+++ backend/src/middleware/isAuth.ts
@@ -5,34 +5,48 @@ import AppError from "../errors/AppError";
 import authConfig from "../config/auth";
 import User from "../models/User";
 
-interface TokenPayload {
-  id: string;
-  username: string;
-  profile: string;
-  companyId: number;
-  iat: number;
-  exp: number;
+const cache: Map<string, {
+  user: any,
+  timestamp: number
+}> = new Map();
+
+async function fetchUserData(userId: string) {
+  if (cache.has(userId)) {
+    const cachedData = cache.get(userId);
+    if (cachedData && (Date.now() - cachedData.timestamp) < 5 * 60 * 1000) { // 5 minutes cache
+      return cachedData.user;
+    }
+  }
+
+  const response = await fetch(process.env.CHECK_AUTH_ENDPOINT, {
+    headers: {
+      'Cookie': `userId=${userId}`,
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3 TKCHAT'
+    }
+  });
+
+  if (!response.ok) {
+   return null;
+  }
+
+  const userData = await response.json();
+  cache.set(userId, { user: userData, timestamp: Date.now() });
+  
+  return userData;
 }
 
 const isAuth = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const solvingUserId = req.cookies['userId'];
-
   if (!solvingUserId) {
     throw new AppError("ERR_SESSION_EXPIRED", 401);
   }
 
-  const solvingUserResponse = await fetch(process.env.CHECK_AUTH_ENDPOINT, {
-    headers: {
-      'Cookie': `userId=${solvingUserId}`,
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3 TKCHAT'
-    }
-  });
-  if (!solvingUserResponse.ok) {
-    logger.error(`Failed to verify user ID: ${solvingUserId}`);
+  const userData = await fetchUserData(solvingUserId);
+  if (!userData) {
+    logger.error(`No user data found for ID: ${solvingUserId}`);
     throw new AppError("ERR_SESSION_EXPIRED", 401);
   }
 
-  const userData = await solvingUserResponse.json();
   if (userData.blocked) {
     logger.error(`User ID ${solvingUserId} is blocked.`);
     throw new AppError("ERR_USER_BLOCKED", 403);
