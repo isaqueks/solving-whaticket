1c80ed55567590b39f8ed68d1b97748d7d6572b6
--unset <iskschluter@gmail.com>
2025-07-14 18:04:08 -0300

    .

--- backend/src/middleware/isAuth.ts
+++ backend/src/middleware/isAuth.ts
@@ -20,7 +20,7 @@ export async function fetchUserData(userId: string) {
 
   const response = await fetch(process.env.CHECK_AUTH_ENDPOINT, {
     headers: {
-      'Cookie': `userId=${userId}`,
+      'Cookie': `user=${userId}`,
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3 TKCHAT'
     }
   });
