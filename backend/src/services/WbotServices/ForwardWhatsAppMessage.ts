27f1fa6066f93149a03b752a5b3e6d115969c717
--unset <iskschluter@gmail.com>
2025-11-01 20:54:25 -0300

    clone instead of fwd

--- backend/src/services/WbotServices/ForwardWhatsAppMessage.ts
+++ backend/src/services/WbotServices/ForwardWhatsAppMessage.ts
@@ -5,6 +5,9 @@ import { getContactJid } from "../../helpers/getContactJid";
 import { getWbot } from "../../libs/wbot";
 import Contact from "../../models/Contact";
 import { Op } from "sequelize";
+import mime from "mime-types";
+import path from "path";
+import fs from 'fs';
 
 interface Request {
   contactId: number;
@@ -35,10 +38,39 @@ const ForwardWhatsAppMessage = async ({
     order: [["createdAt", "ASC"]],
   });
 
+  const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+  const publicFolder = path.resolve(__dirname, "..", "..", "..", "public");
+  const jid = getContactJid(contact);
   for (const message of messages) {
-    await wbot.sendMessage(getContactJid(contact), {
-      forward: JSON.parse(message.dataJson) as WAMessage
-    });
+    if (['conversation', 'editedMessage', 'extendedTextMessage'].includes(message.mediaType)) {
+      await wbot.sendMessage(jid, { text: message.body });
+    }
+    else if (['audio', 'image', 'application', 'video'].includes(message.mediaType)) {
+      const mimeType = mime.lookup(message.mediaUrl);
+      const opt = {
+        fileName: message.mediaUrl.split('/').pop(),
+        caption: message.body,
+        mimetype: mimeType || 'application'
+      } as any;
+
+      if (message.mediaType === 'audio') {
+        opt.audio = fs.createReadStream(`${publicFolder}/${message.mediaUrl}`);
+        opt.ptt = message.mediaType === 'audio' && mime.lookup(message.mediaUrl) === 'audio/ogg' ? true : false;
+      } else if (message.mediaType === 'image') {
+        opt.image = fs.createReadStream(`${publicFolder}/${message.mediaUrl}`);
+      } else if (message.mediaType === 'application') {
+        opt.document = fs.createReadStream(`${publicFolder}/${message.mediaUrl}`);
+      } else if (message.mediaType === 'video') {
+        opt.video = fs.createReadStream(`${publicFolder}/${message.mediaUrl}`);
+      }
+
+      await wbot.sendMessage(jid, {
+        ...opt,
+      });
+
+      await sleep(1000); // wait 1 second between messages to avoid spamming
+    }
   }
   
 };
