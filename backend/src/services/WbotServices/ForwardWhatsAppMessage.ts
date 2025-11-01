200352980062014d826c4cc7160b950b88462789
--unset <iskschluter@gmail.com>
2025-11-01 20:58:22 -0300

    fix

--- backend/src/services/WbotServices/ForwardWhatsAppMessage.ts
+++ backend/src/services/WbotServices/ForwardWhatsAppMessage.ts
@@ -43,26 +43,29 @@ const ForwardWhatsAppMessage = async ({
   const publicFolder = path.resolve(__dirname, "..", "..", "..", "public");
   const jid = getContactJid(contact);
   for (const message of messages) {
+
+    const fileName = message.mediaUrl ? message.mediaUrl.split('/').pop() : null;
+
     if (['conversation', 'editedMessage', 'extendedTextMessage'].includes(message.mediaType)) {
       await wbot.sendMessage(jid, { text: message.body });
     }
     else if (['audio', 'image', 'application', 'video'].includes(message.mediaType)) {
       const mimeType = mime.lookup(message.mediaUrl);
       const opt = {
-        fileName: message.mediaUrl.split('/').pop(),
+        fileName: fileName,
         caption: message.body,
         mimetype: mimeType || 'application'
       } as any;
 
       if (message.mediaType === 'audio') {
-        opt.audio = fs.createReadStream(`${publicFolder}/${message.mediaUrl}`);
+        opt.audio = fs.createReadStream(`${publicFolder}/${fileName}`);
         opt.ptt = message.mediaType === 'audio' && mime.lookup(message.mediaUrl) === 'audio/ogg' ? true : false;
       } else if (message.mediaType === 'image') {
-        opt.image = fs.createReadStream(`${publicFolder}/${message.mediaUrl}`);
+        opt.image = fs.createReadStream(`${publicFolder}/${fileName}`);
       } else if (message.mediaType === 'application') {
-        opt.document = fs.createReadStream(`${publicFolder}/${message.mediaUrl}`);
+        opt.document = fs.createReadStream(`${publicFolder}/${fileName}`);
       } else if (message.mediaType === 'video') {
-        opt.video = fs.createReadStream(`${publicFolder}/${message.mediaUrl}`);
+        opt.video = fs.createReadStream(`${publicFolder}/${fileName}`);
       }
 
       await wbot.sendMessage(jid, {
